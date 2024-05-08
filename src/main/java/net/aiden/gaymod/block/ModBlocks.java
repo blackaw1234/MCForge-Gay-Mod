9dbb59129b8c88d5796d7a2880b3dcd2e83e7d67
Aiden Black <blackaw1234@gmail.com>
2024-05-08 01:27:03 -0500

    Remove unused block STICKY_SUPERCHARGED_PISTON_HEAD_BLOCK

--- src/main/java/net/aiden/gaymod/block/ModBlocks.java
+++ src/main/java/net/aiden/gaymod/block/ModBlocks.java
@@ -34,10 +34,6 @@ public class ModBlocks {
             () -> new SuperchargedPistonHeadBlock( BlockBehaviour.Properties.of(Material.PISTON)
                     .strength(6f)), null);
 
-    public static final RegistryObject<Block> STICKY_SUPERCHARGED_PISTON_HEAD_BLOCK = registerBlock("supercharged_piston_head_sticky",
-            () -> new SuperchargedPistonHeadBlock( BlockBehaviour.Properties.of(Material.PISTON)
-                    .strength(6f).requiresCorrectToolForDrops()), null);
-
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab)
     {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
